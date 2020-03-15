dba949630147de813c439d579d7c0c29bb3a00ac
minopanda <mino_panda3412@yahoo.co.jp>
2020-03-15 19:09:49 +0900

    [add]orderのモデル・コントローラー・ビュー追加しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'orders/new'
+  get 'orders/confirm'
+  get 'orders/success'
+  get 'orders/index'
+  get 'orders/show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_03_14_102849) do
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "name", default: ""
+    t.string "postal_code", default: ""
+    t.string "address", default: ""
+    t.integer "payment"
+    t.integer "total_price", null: false
+    t.integer "postage", default: 800
+    t.string "status", default: "入金待ち"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
 end
