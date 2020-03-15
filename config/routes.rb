dec655348430c9d9b36b45bc00015fb2e8faf172
1noseA <1noseA2L@gmail.com>
2020-03-15 19:23:59 +0900

    [Remove]マージのためroute削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users
-  resources :products,only:[:index,:show] do
-    resource :carts,only:[:show,:create,:update,:destroy]
-  end
+
 
 
 end
