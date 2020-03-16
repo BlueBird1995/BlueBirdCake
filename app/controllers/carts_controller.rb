509bd3bec25decaeb97d3b9c488f14e8ceed295a
1noseA <1noseA2L@gmail.com>
2020-03-16 12:11:36 +0900

    [Add]cartのビュー追加

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,8 @@
 class CartsController < ApplicationController
   def show
+  	@product = Product.find(params[:id])
+  	@cart_product = Cart.new
+  	@cart_products = @product.cart_products
   end
 
   def create
