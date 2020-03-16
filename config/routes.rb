8249a7feecf32fb394dea5b8e6d6e79c9ba25af2
今枝光 <hikaru.fc0710@gmail.com>
2020-03-16 03:13:39 +0000

    ユーザー機能概ね実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root 'homes#top'
   devise_for :users
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update] do
+    #本当に削除しますかページ
+    member do
+      get :confirm_destroy
+    end
+  end
+  #ユーザの論理削除するためのroute
+  put "/users/:id/hide" => "users#hide", as: 'users_hide'
 end
