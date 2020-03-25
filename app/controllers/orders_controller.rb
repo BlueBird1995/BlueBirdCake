9f50bce72fdf1bb9974355d30692349612742897
yuki-snow1823 <magic.iroha@gmail.com>
2020-03-25 19:14:53 +0900

    [finish]バリデーション

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-
+  before_action :authenticate_user!
   def new #注文者情報を入力する
     # test
     @order = Order.new
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,15 @@
 class Product < ApplicationRecord
 	has_many :carts, dependent: :destroy
-	has_many :ordered_products
+    has_many :ordered_products, dependent: :destroy
+    validates :genre_id, presence: true 
+    validates :product_image_id,presence: true 
+    validates :price, presence: true 
+    validates :description, presence: true 
+    validates :status, presence: true 
+    validates :name, presence: true 
+    validates :created_at, presence: true 
+    validates :updated_at, presence: true 
+
 
 	#商品の税込み単価
     def tax_included
