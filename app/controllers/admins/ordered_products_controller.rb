d94312281c9d8e64e3d562045a25c80497337345
minopanda <mino_panda3412@yahoo.co.jp>
2020-03-25 16:03:38 +0900

    [fix]ファビコンとorder少し微調整

--- app/controllers/admins/ordered_products_controller.rb
+++ app/controllers/admins/ordered_products_controller.rb
@@ -3,6 +3,7 @@ class Admins::OrderedProductsController < ApplicationController
 	def update #製作ステータスの更新
 		@ordered_product = OrderedProduct.find(params[:id])
 		@ordered_product.update(ordered_product_params)
+		redirect_back(fallback_location: root_path)
     end
 
 private
